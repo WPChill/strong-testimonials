ed6a151ff12978ba6d90888982e016a619137286
Chris Dillon <chrisdillon.us@gmail.com>
2014-06-12 14:58:47 -0400

    Disable update notice on custom version

--- strong-testimonials.php
+++ strong-testimonials.php
@@ -32,6 +32,13 @@
 
 define( 'WPMTST_NAME', 'wpmtst' );
 
+// disable update notice on custom version
+function remove_update_notification( $value ) {
+	unset( $value->response[ plugin_basename( __FILE__ ) ] );
+	return $value;
+}
+add_filter( 'site_transient_update_plugins', 'remove_update_notification' );
+
 
 /*
 	Text domain.
