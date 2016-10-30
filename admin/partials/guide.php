de4ab12dec697be4d4482dc8b0ff62647f3794b0
Chris Dillon <chrisdillon.us@gmail.com>
2016-10-30 09:11:44 -0400

    update guide

--- admin/partials/guide.php
+++ admin/partials/guide.php
@@ -1,58 +1,26 @@
 <?php
 /**
- * The Guide pages.
+ * The Guide page.
  *
  * @since 1.9.1
  * @package Strong_Testimonials
  */
 
 function wpmtst_guide() {
-	$tab  = isset( $_GET['tab'] ) ? $_GET['tab'] : 'welcome';
-	$page = admin_url( 'edit.php?post_type=wpm-testimonial&page=testimonial-guide');
-	$tags = array( 'a' => array( 'href' => array(), 'target' => array(), 'class' => array() ), 'br' => array() );
+	$tags           = array( 'a' => array( 'href' => array(), 'target' => array(), 'class' => array() ), 'br' => array() );
+	$plugin_data    = WPMST()->get_plugin_data();
+	$plugin_version = $plugin_data['Version'];
+	$major_minor    = strtok( $plugin_version, '.' ) . '.' . strtok( '.' );
 	?>
-	<div class="wrap wpmtst guide">
-		<!--<h1>--><?php //_e( 'Strong Testimonials Guide', 'strong-testimonials' ); ?><!--</h1>-->
-		<?php /* translators: In the Guide. */ ?>
-		<h2 class="nav-tab-wrapper">
-			<a href="<?php echo $page; ?>" class="nav-tab <?php echo $tab == 'welcome' ? 'nav-tab-active' : ''; ?>">
-				<?php _e( 'Welcome', 'strong-testimonials' ); ?>
-			</a>
-			<a href="<?php echo $page; ?>&tab=start" class="nav-tab <?php echo $tab == 'start' ? 'nav-tab-active' : ''; ?>">
-				<?php _e( 'Getting Started', 'strong-testimonials' ); ?>
-			</a>
-			<a href="<?php echo $page; ?>&tab=views" class="nav-tab <?php echo $tab == 'views' ? 'nav-tab-active' : ''; ?>">
-				<?php _e( 'Views', 'strong-testimonials' ); ?>
-			</a>
-			<a href="<?php echo $page; ?>&tab=translation" class="nav-tab <?php echo $tab == 'translation' ? 'nav-tab-active' : ''; ?>">
-				<?php _e( 'Translation', 'strong-testimonials' ); ?>
-			</a>
-		</h2>
-		<?php
-		switch ( $tab ) {
-			case 'translation':
-				include 'guide/translation.php';
-				break;
-			case 'views':
-				include 'guide/views.php';
-				break;
-			case 'start':
-				include 'guide/start.php';
-				break;
-			default:
-				include 'guide/welcome.php';
-		}
-		?>
+	<div class="wrap wpmtst-guide">
+
+		<h1 class="large"><?php printf( __( 'Welcome to Strong Testimonials %s', 'strong-testimonials' ), $major_minor ); ?></h1>
+
+		<div class="guide-wrapper">
+			<?php include 'guide/about.php'; ?>
+			<?php include 'guide/sidebar.php'; ?>
+		</div>
+
 	</div>
 	<?php
 }
-
-function wpmtst_guide_before_content() {
-	include 'guide/plugin-sidebar.php';
-}
-add_action( 'wpmtst_guide_before_content', 'wpmtst_guide_before_content' );
-
-function wpmtst_guide_after_content() {
-	//include 'plugin-sidebar.php';
-}
-add_action( 'wpmtst_guide_after_content', 'wpmtst_guide_after_content' );
---
+++ admin/partials/guide/about.php
@@ -0,0 +1,60 @@
+<div id="about">
+
+	<div class="section">
+
+		<p>This may be a little different than others you've tried. There is only one shortcode and one widget.</p>
+
+		<h2><?php _e( 'Views make it simpler', 'strong-testimonials' ); ?></h2>
+
+		<p>First, create a <strong>view</strong> for anything you want to display. A view is simply of group of settings. You can create an unlimited number of views.</p>
+
+		<p>For example, you might have one view to <strong>display a grid</strong> of your testimonials, another view for a testimonial <strong>submission form</strong>, and another view for a <strong>slideshow</strong>.</p>
+
+		<p>Then add the view to your site using its unique shortcode or by selecting the view in the widget.</p>
+
+	</div>
+
+	<div class="section">
+
+		<h2><?php _e( 'What\'s new' ); ?></h2>
+
+		<p><span class="dashicons dashicons-star-filled"></span>Star ratings.</p>
+
+		<p><span class="dashicons dashicons-admin-page"></span>Use standard WordPress pagination just like standard blog posts.</p>
+
+		<p><span class="dashicons dashicons-edit"></span>A redesigned fields section in the view editor.</p>
+
+		<p><span class="dashicons dashicons-editor-paste-text"></span>Display some text before a field; for example "Service" or "Rating".</p>
+
+		<p><span class="dashicons dashicons-performance"></span>Improved compatibility and performance.</p>
+
+	</div>
+
+	<div class="section">
+
+		<h2><?php _e( 'Translation', 'strong-testimonials' ); ?></h2>
+
+		<p><?php _e( 'Strong Testimonials is compatible with WPML and Polylang.', 'strong-testimonials' ); ?></p>
+
+		<p><?php _e( 'In each plugin, domains are added to the <strong>String Translation</strong> screens. Those domains encompass the form fields, the form messages, the notification email, and the "Read more" link text in your views. They are updated automatically when your form fields or settings change.', 'strong-testimonials' ); ?></p>
+
+	</div>
+
+	<div class="section">
+
+		<h2><?php _e( 'Troubleshooting' ); ?></h2>
+
+		<h3><?php _e( 'Try clearing your caches' ); ?></h3>
+
+		<p>Why? CSS and JavaScript files change occasionally. The plugin tries to load the latest version of each file (i.e. bust the cache) but sometimes caches still need to be cleared. That includes caching and minifying plugins, server-side caching, and your individual browsers.</p>
+
+		<p><strong>Pro Tip:</strong> Make it a habit to clear your caches after updating any plugin with front-end output.</p>
+
+		<h3><?php _e( 'Upgrading from version 1?' ); ?></h3>
+
+		<p>The &#91;strong&#93;, &#91;wpmtst&#93; and &#91;read_more&#93; shortcodes and the original widget <span
+				style="color: red;">were removed</span> in version 2.0. <a href="https://www.wpmission.com/strong-testimonials-version-2-coming-soon/" target="_blank">Here's why</a>. <em>I promise this is the last major change.</em> Everything now happens in <strong>views</strong>.</p>
+
+	</div>
+
+</div>
