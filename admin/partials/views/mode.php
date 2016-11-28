cef449ae93701e0a351d93dc53f6723c3aa859c1
Chris Dillon <chrisdillon.us@gmail.com>
2016-11-28 12:20:26 -0500

    Replace hardcoded mode options in view editor

--- admin/partials/views/mode.php
+++ admin/partials/views/mode.php
@@ -2,7 +2,7 @@
 <div class="view-info mode-select">
 	<div id="view-mode" class="large">
 		<span class="title"><?php _e( 'Mode:', 'strong-testimonials' ); ?></span>
-		<?php foreach ( $view_options['mode']['options'] as $mode ) : ?>
+		<?php foreach ( $view_options['mode'] as $mode ) : ?>
 			<label>
 				<input id="<?php echo $mode['name']; ?>" type="radio" name="view[data][mode]"
 					   value="<?php echo $mode['name']; ?>" <?php checked( $view['mode'], $mode['name'] ); ?>>
