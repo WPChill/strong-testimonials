86537eddba2bc14fd6d138c82f62233dad12cffb
Chris Dillon <chrisdillon.us@gmail.com>
2016-12-10 07:39:14 -0500

    Optimize fields Ajax functions

--- admin/custom-fields-ajax.php
+++ admin/custom-fields-ajax.php
@@ -10,7 +10,6 @@
 function wpmtst_add_field_function() {
 	check_ajax_referer( 'wpmtst-admin', 'security', false );
 
-	$new_key = intval( $_REQUEST['nextKey'] );
 	// when adding, leave Name empty so it will be populated from Label
 	$empty_field = array(
 		'name'         => '',
@@ -20,8 +19,7 @@ function wpmtst_add_field_function() {
 		'label'        => __( 'New Field', 'strong-testimonials' ),
 		'show_label'   => 1,
 	);
-	$new_field = wpmtst_show_field( $new_key, $empty_field, true );
-	echo $new_field;
+	echo wpmtst_show_field( intval( $_REQUEST['nextKey'] ), $empty_field, true );
 	die();
 }
 add_action( 'wp_ajax_wpmtst_add_field', 'wpmtst_add_field_function' );
@@ -33,7 +31,6 @@ add_action( 'wp_ajax_wpmtst_add_field', 'wpmtst_add_field_function' );
 function wpmtst_add_field_2_function() {
 	check_ajax_referer( 'wpmtst-admin', 'security', false );
 
-	$new_key         = intval( $_REQUEST['nextKey'] );
 	$new_field_type  = $_REQUEST['fieldType'];
 	$new_field_class = $_REQUEST['fieldClass'];
 	$fields          = get_option( 'wpmtst_fields' );
@@ -41,8 +38,7 @@ function wpmtst_add_field_2_function() {
 		$fields['field_types'][$new_field_class][$new_field_type],
 		array( 'record_type' => $new_field_class )
 	);
-	$new_field = wpmtst_show_field_secondary( $new_key, $empty_field );
-	echo $new_field;
+	echo wpmtst_show_field_secondary( intval( $_REQUEST['nextKey'] ), $empty_field );
 	die();
 }
 add_action( 'wp_ajax_wpmtst_add_field_2', 'wpmtst_add_field_2_function' );
@@ -54,7 +50,6 @@ add_action( 'wp_ajax_wpmtst_add_field_2', 'wpmtst_add_field_2_function' );
 function wpmtst_add_field_3_function() {
 	check_ajax_referer( 'wpmtst-admin', 'security', false );
 
-	$new_key         = intval( $_REQUEST['nextKey'] );
 	$new_field_type  = $_REQUEST['fieldType'];
 	$new_field_class = $_REQUEST['fieldClass'];
 	$fields          = get_option( 'wpmtst_fields' );
@@ -62,8 +57,7 @@ function wpmtst_add_field_3_function() {
 		$fields['field_types'][$new_field_class][$new_field_type],
 		array( 'record_type' => $new_field_class )
 	);
-	$new_field = wpmtst_show_field_hidden( $new_key, $empty_field );
-	echo $new_field;
+	echo wpmtst_show_field_hidden( intval( $_REQUEST['nextKey'] ), $empty_field );
 	die();
 }
 add_action( 'wp_ajax_wpmtst_add_field_3', 'wpmtst_add_field_3_function' );
@@ -75,7 +69,6 @@ add_action( 'wp_ajax_wpmtst_add_field_3', 'wpmtst_add_field_3_function' );
 function wpmtst_add_field_4_function() {
 	check_ajax_referer( 'wpmtst-admin', 'security', false );
 
-	$new_key         = intval( $_REQUEST['nextKey'] );
 	$new_field_type  = $_REQUEST['fieldType'];
 	$new_field_class = $_REQUEST['fieldClass'];
 	$fields          = get_option( 'wpmtst_fields' );
@@ -86,8 +79,7 @@ function wpmtst_add_field_4_function() {
 			array( 'record_type' => $new_field_class )
 		);
 	}
-	$new_field = wpmtst_show_field_admin_table( $new_key, $empty_field );
-	echo $new_field;
+	echo wpmtst_show_field_admin_table( intval( $_REQUEST['nextKey'] ), $empty_field );
 	die();
 }
 add_action( 'wp_ajax_wpmtst_add_field_4', 'wpmtst_add_field_4_function' );
