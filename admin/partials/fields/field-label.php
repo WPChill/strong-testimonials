d8a4449fdafb0b74bf1b9c86f4e34bcf28638ae1
Chris Dillon <chrisdillon.us@gmail.com>
2016-12-10 07:38:01 -0500

    Refactor field HTML into partials

---
+++ admin/partials/fields/field-label.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Field Label
+ */
+?>
+<tr class="field-label-row">
+	<th><?php _ex( 'Label', 'noun', 'strong-testimonials' ); ?></th>
+	<td>
+		<input type="text" class="field-label"
+			   name="fields[<?php echo $key; ?>][label]"
+			   value="<?php echo wpmtst_htmlspecialchars( $field['label'] ); ?>">
+		<span class="help"><?php _e( 'This appears on the form.', 'strong-testimonials' ); ?></span>
+		<label>
+			<span class="help">
+				<input type="checkbox" name="fields[<?php echo $key; ?>][show_label]"
+					<?php checked( $field['show_label'], true ); ?>><?php _e( 'Show this label on the form.', 'strong-testimonials' ); ?>
+			</span>
+		</label>
+	</td>
+</tr>
