4f164ad090ed8243aac4f40130121c1c138551a1
Chris Dillon <chrisdillon.us@gmail.com>
2017-02-15 15:05:15 -0500

    Minor UI tweaks

--- admin/partials/views/option-form-ajax.php
+++ admin/partials/views/option-form-ajax.php
@@ -3,7 +3,7 @@
 	<input type="checkbox" id="view-form_ajax" class="checkbox if toggle"
            name="view[data][form_ajax]" value="1" <?php checked( $view['form_ajax'] ); ?>>
 	<label for="view-form_ajax">
-		<?php _e( 'Submit form via Ajax', 'strong-testimonials' ); ?>
+		<?php _e( 'Submit form without reloading the page (Ajax)', 'strong-testimonials' ); ?>
 	</label>
 </th>
 <td>
