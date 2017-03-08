6f4873529aca3368e42592af8f34d5f5c65bd8d3
Chris Dillon <chrisdillon.us@gmail.com>
2017-03-08 07:02:50 -0500

    Update guide and sidebar

--- admin/partials/guide/guide.php
+++ admin/partials/guide/guide.php
@@ -2,30 +2,68 @@
 
 	<div class="section">
 
-		<h2>Looking for shortcodes?</h2>
+		<h2>How to get started</h2>
 
 		<p>
-			<?php _e( '<strong>You must create a view first.</strong>', 'strong-testimonials' ); ?>
-			<?php _e( 'A view is simply of group of settings for what you want to display. ', 'strong-testimonials' ); ?>
+			<?php _e( 'This plugin is different than others you may have tried.', 'strong-testimonials' ); ?>
+            <strong><?php
+			printf(
+				wp_kses( __( 'You must create a <a href="%s">view</a> first.', 'strong-testimonials' ), $tags ),
+				admin_url( 'edit.php?post_type=wpm-testimonial&page=testimonial-views' )
+			);
+			?></strong>
+			<?php _e( 'A view is simply of group of settings with an easy-to-use editor. ', 'strong-testimonials' ); ?>
 		</p>
-
-		<p>
-			<?php _e( 'Instead of learning multiple shortcodes with dozens of options, a view contains all the options in an easy-to-use editor.', 'strong-testimonials' ); ?>
-			<?php _e( 'Display the view using its simple shortcode (which you will see after you save it) or the widget.', 'strong-testimonials' ); ?>
-		</p>
-
 		<p>
 			<?php _e( 'You can create an unlimited number of views.', 'strong-testimonials' ); ?>
-			<?php _e( 'For example, you might have one view to <strong>display a grid</strong> of your testimonials, another view for a testimonial <strong>submission form</strong>, and another view for a <strong>slideshow</strong>.', 'strong-testimonials' ); ?>
+			<?php _e( 'For example:', 'strong-testimonials' ); ?>
+        </p>
+        <ul>
+        <li><?php _e( 'a view to <b>display</b> your testimonials in a list or a grid', 'strong-testimonials' ); ?></li>
+        <li><?php _e( 'another view to display testimonials from a specific <b>category</b>', 'strong-testimonials' ); ?></li>
+        <li><?php _e( 'another view for the testimonial submission <b>form</b>', 'strong-testimonials' ); ?></li>
+        <li><?php _e( 'another view for a <b>slideshow</b> widget', 'strong-testimonials' ); ?></li>
+        </ul>
+        <p>
+			<?php _e( 'Add the view using its unique shortcode (which you will see after you save it) or the widget.', 'strong-testimonials' ); ?>
 		</p>
+
 
 	</div>
 
 	<hr>
 
-	<div class="section">
+    <div class="section">
 
-		<h2><?php _e( 'What\'s new' ); ?></h2>
+        <h2><?php _e( 'How to display the number of testimonials', 'strong-testimonials' ); ?></h2>
+
+        <p>
+			<?php printf( __( 'Use the %s shortcode.', 'strong-testimonials' ), '<code>&#91;testimonial_count&#93;</code>' ); ?>
+            <?php _e( 'For example:', 'strong-testimonials' ); ?>
+        </p>
+        <p>
+            <span style="background: white; font-family: Consolas, Monaco, monospace; padding: 3px 8px; display: inline-block;">
+			    <?php printf( __( 'Read some of our %s testimonials!', 'strong-testimonials' ), '&#91;testimonial_count&#93;' ); ?>
+            </span>
+        </p>
+
+        <p>
+			<?php printf( __( 'To count for a specific category, add the %s attribute with the category slug.', 'strong-testimonials' ), '<code>category</code>' ); ?>
+			<?php _e( 'For example:', 'strong-testimonials' ); ?>
+        </p>
+        <p>
+            <span style="background: white; font-family: Consolas, Monaco, monospace; padding: 3px 8px; display: inline-block;">
+			    <?php printf( __( 'Here\'s what %s local clients say', 'strong-testimonials' ), '&#91;testimonial_count category="local"&#93;' ); ?>
+            </span>
+        </p>
+
+    </div>
+
+    <hr>
+
+    <div class="section">
+
+		<h2><?php _e( 'Recently added features' ); ?></h2>
 
         <p><span class="dashicons dashicons-external"></span><?php _e( 'Form success redirect options.', 'strong-testimonials' ); ?></p>
 
@@ -35,13 +73,13 @@
 
 		<p><span class="dashicons dashicons-star-filled"></span><?php _e( 'Star ratings.', 'strong-testimonials' ); ?></p>
 
-		<p><span class="dashicons dashicons-admin-page"></span><?php _e( 'Use standard WordPress pagination just like standard blog posts.', 'strong-testimonials' ); ?></p>
-
 		<p><span class="dashicons dashicons-performance"></span><?php _e( 'Improved compatibility and performance.', 'strong-testimonials' ); ?></p>
+
+        <p></p>
 
 	</div>
 
-	<hr>
+    <hr>
 
 	<div class="section">
 
@@ -63,31 +101,8 @@
 
 		<p><?php
 		printf(
-			wp_kses(
-				__( 'The &#91;strong&#93;, &#91;wpmtst&#93; and &#91;read_more&#93; shortcodes and the original widget were <a href="%s" target="_blank">removed in version 2</a>. Everything now happens in <strong>views</strong>. <em>I promise this is the last major change.</em>', 'strong-testimonials' ),
-				array(
-					'a'      => array( 'href' => array(), 'target' => array(), 'class' => array() ),
-					'br'     => array(),
-					'em'     => array(),
-					'strong' => array(),
-				)
-			),
+			wp_kses( __( 'The original shortcodes and widget were <a href="%s" target="_blank">removed in version 2</a>. Everything now happens in <strong>views</strong>.', 'strong-testimonials' ), $tags ),
 			esc_url( 'https://www.wpmission.com/strong-testimonials-version-2-coming-soon/' )
-		);
-		?></p>
-
-		<p><?php
-		printf(
-			wp_kses(
-				__( 'Visit <a href="%s" target="_blank">WP Mission</a> for more documentation.', 'strong-testimonials' ),
-				array(
-					'a'      => array( 'href' => array(), 'target' => array(), 'class' => array() ),
-					'br'     => array(),
-					'em'     => array(),
-					'strong' => array(),
-				)
-			),
-			esc_url( 'https://www.wpmission.com' )
 		);
 		?></p>
 
