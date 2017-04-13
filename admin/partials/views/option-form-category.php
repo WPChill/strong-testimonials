f29d6ebefe4a68b5fc8b86580b4c0709485a7a0a
Chris Dillon <chrisdillon.us@gmail.com>
2017-04-13 12:08:30 -0400

    Add expand/collapse button to form view category list

--- admin/partials/views/option-form-category.php
+++ admin/partials/views/option-form-category.php
@@ -2,16 +2,25 @@
 <th>
 	<?php _e( 'Assign new submissions to a category', 'strong-testimonials' ); ?>
 </th>
-<td class="valign-middle">
-	<?php if ( $category_list ) : ?>
-	<div class="table">
-		<div class="table-row">
-			<div class="table-cell">
-				<?php wpmtst_form_category_checklist( $view_cats_array ); ?>
-			</div>
-		</div>
-	</div>
-	<?php else : ?>
-		<p class="description tall"><?php _e( 'No categories found', 'strong-testimonials' ); ?></p>
-	<?php endif; ?>
-</td>
+<?php if ( $category_list ) : ?>
+    <td>
+        <div class="table">
+            <div class="table-row">
+                <div class="table-cell">
+                    <div class="row" style="text-align: right; padding-bottom: 5px;">
+                        <input type="button" id="expand-cats" class="button" value="expand list"/>
+                    </div>
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+					<?php wpmtst_form_category_checklist( $view_cats_array ); ?>
+                </div>
+            </div>
+        </div>
+    </td>
+<?php else : ?>
+    <td>
+        <p class="description tall"><?php _e( 'No categories found', 'strong-testimonials' ); ?></p>
+    </td>
+<?php endif; ?>
