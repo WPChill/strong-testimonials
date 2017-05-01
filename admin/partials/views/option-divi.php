57abfa23dbcbaa282f90d4889f68012dba874be5
Chris Dillon <chrisdillon.us@gmail.com>
2017-05-01 04:22:59 -0400

    Minor admin UI

--- admin/partials/views/option-divi.php
+++ admin/partials/views/option-divi.php
@@ -13,7 +13,7 @@ global $view;
             <label for="view-divi_builder">
                 <?php _e( 'Check this if using Divi Builder version 2+', 'strong-testimonials-lucid-theme' ); ?>
             </label>
-            <p class="description">
+            <p class="description short">
                 <?php _e( 'This is only required if adding this view using the Visual Builder in <b>version 2</b> of the Divi Builder plugin by Elegant Themes.', 'strong-testimonials-lucid-theme' ); ?>
             </p>
         </div>
