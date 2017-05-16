b9212aeb3bf628a85414958a70b2bdbc67a57c31
Chris Dillon <chrisdillon.us@gmail.com>
2017-05-16 07:04:32 -0400

    Formatting

--- includes/scripts.php
+++ includes/scripts.php
@@ -13,42 +13,77 @@ function wpmtst_scripts() {
 	/**
 	 * Fonts
 	 */
-	wp_register_style( 'wpmtst-font-awesome', WPMTST_PUBLIC_URL . 'fonts/font-awesome-4.6.3/css/font-awesome.min.css', array(), '4.6.3' );
+	wp_register_style( 'wpmtst-font-awesome',
+		WPMTST_PUBLIC_URL . 'fonts/font-awesome-4.6.3/css/font-awesome.min.css',
+		array(),
+		'4.6.3' );
 
 	/**
 	 * Simple pagination
 	 */
-	wp_register_script( 'wpmtst-pager-plugin', WPMTST_PUBLIC_URL . 'js/jquery.quickpager.js', array( 'jquery' ), false, true );
-	wp_register_script( 'wpmtst-pager-script', WPMTST_PUBLIC_URL . 'js/pager.js', array( 'wpmtst-pager-plugin' ), $plugin_version, true );
+	wp_register_script( 'wpmtst-pager-plugin',
+		WPMTST_PUBLIC_URL . 'js/jquery.quickpager.js',
+		array( 'jquery' ),
+		false,
+		true );
+
+	wp_register_script( 'wpmtst-pager-script',
+		WPMTST_PUBLIC_URL . 'js/pager.js',
+		array( 'wpmtst-pager-plugin' ),
+		$plugin_version,
+		true );
 
 	/**
 	 * View custom style
 	 */
-	wp_register_style( 'wpmtst-custom-style', WPMTST_PUBLIC_URL . 'css/custom.css' );
+	wp_register_style( 'wpmtst-custom-style',
+		WPMTST_PUBLIC_URL . 'css/custom.css' );
 
 	/**
 	 * imagesLoaded, if less than WordPress 4.6
 	 */
 	if ( ! wp_script_is( 'imagesloaded', 'registered' ) ) {
-		wp_register_script( 'imagesloaded', WPMTST_PUBLIC_URL . 'js/lib/imagesloaded/imagesloaded.pkgd.min.js', array(), '3.2.0', true );
+		wp_register_script( 'imagesloaded',
+			WPMTST_PUBLIC_URL . 'js/lib/imagesloaded/imagesloaded.pkgd.min.js',
+			array(),
+			'3.2.0',
+			true );
 	}
 
 	/**
 	 * Masonry
 	 */
-	wp_register_script( 'wpmtst-masonry-script', WPMTST_PUBLIC_URL . 'js/masonry.js', array( 'jquery-masonry', 'imagesloaded' ), $plugin_version, true );
-	wp_register_style( 'wpmtst-masonry-style', WPMTST_PUBLIC_URL . 'css/masonry.css', array(), $plugin_version );
+	wp_register_script( 'wpmtst-masonry-script',
+		WPMTST_PUBLIC_URL . 'js/masonry.js', array( 'jquery-masonry', 'imagesloaded' ),
+		$plugin_version,
+		true );
+
+	wp_register_style( 'wpmtst-masonry-style',
+		WPMTST_PUBLIC_URL . 'css/masonry.css',
+		array(),
+		$plugin_version );
 
 	/**
 	 * Columns
 	 */
-	wp_register_style( 'wpmtst-columns-style', WPMTST_PUBLIC_URL . 'css/columns.css', array(), $plugin_version );
+	wp_register_style( 'wpmtst-columns-style',
+		WPMTST_PUBLIC_URL . 'css/columns.css',
+		array(),
+		$plugin_version );
 
 	/**
 	 * Grid
 	 */
-	wp_register_script( 'wpmtst-grid-script', WPMTST_PUBLIC_URL . 'js/grid.js', array( 'jquery' ), $plugin_version, true );
-	wp_register_style( 'wpmtst-grid-style', WPMTST_PUBLIC_URL . 'css/grid.css', array(), $plugin_version );
+	wp_register_script( 'wpmtst-grid-script',
+		WPMTST_PUBLIC_URL . 'js/grid.js',
+		array( 'jquery' ),
+		$plugin_version,
+		true );
+
+	wp_register_style( 'wpmtst-grid-style',
+		WPMTST_PUBLIC_URL . 'css/grid.css',
+		array(),
+		$plugin_version );
 
 	/**
 	 * Ratings
@@ -57,15 +92,31 @@ function wpmtst_scripts() {
 	if ( isset( $options['load_font_awesome'] ) && $options['load_font_awesome'] ) {
 		$deps = array( 'wpmtst-font-awesome' );
 	}
-	wp_register_style( 'wpmtst-rating-form', WPMTST_PUBLIC_URL . 'css/rating-form.css', $deps, $plugin_version );
-	wp_register_style( 'wpmtst-rating-display', WPMTST_PUBLIC_URL . 'css/rating-display.css', $deps, $plugin_version );
+
+	wp_register_style( 'wpmtst-rating-form',
+		WPMTST_PUBLIC_URL . 'css/rating-form.css',
+		$deps,
+		$plugin_version );
+
+	wp_register_style( 'wpmtst-rating-display',
+		WPMTST_PUBLIC_URL . 'css/rating-display.css',
+		$deps,
+		$plugin_version );
 
 	/**
 	 * Form handling
 	 */
-	wp_register_script( 'wpmtst-validation-plugin', WPMTST_PUBLIC_URL . 'js/lib/validate/jquery.validate.min.js', array( 'jquery' ), '1.13.1', true );
+	wp_register_script( 'wpmtst-validation-plugin',
+		WPMTST_PUBLIC_URL . 'js/lib/validate/jquery.validate.min.js',
+		array( 'jquery' ),
+		'1.13.1',
+		true );
 
-	wp_register_script( 'wpmtst-form-validation', WPMTST_PUBLIC_URL . 'js/form-validation.js', array( 'wpmtst-validation-plugin', 'jquery-form' ), $plugin_version, true );
+	wp_register_script( 'wpmtst-form-validation',
+		WPMTST_PUBLIC_URL . 'js/form-validation.js',
+		array( 'wpmtst-validation-plugin', 'jquery-form' ),
+		$plugin_version,
+		true );
 
 	/**
 	 * Localize jQuery Validate plugin.
@@ -77,21 +128,41 @@ function wpmtst_scripts() {
 		$lang_parts = explode( '_', $locale );
 		$lang_file  = 'js/lib/validate/localization/messages_' . $lang_parts[0] . '.min.js';
 		if ( file_exists( WPMTST_PUBLIC . $lang_file ) ) {
-			wp_register_script( 'wpmtst-validation-lang', WPMTST_PUBLIC_URL . $lang_file, array( 'wpmtst-validation-plugin' ), false, true );
+			wp_register_script( 'wpmtst-validation-lang',
+				WPMTST_PUBLIC_URL . $lang_file,
+				array( 'wpmtst-validation-plugin' ),
+				false,
+				true );
 		}
 	}
 
 	/**
 	 * Slider
 	 */
-	wp_register_script( 'jquery-actual', WPMTST_PUBLIC_URL . "js/lib/actual/jquery.actual{$min}.js", array( 'jquery' ), '1.0.16', true );
+	wp_register_script( 'jquery-actual',
+		WPMTST_PUBLIC_URL . "js/lib/actual/jquery.actual{$min}.js",
+		array( 'jquery' ),
+		'1.0.16',
+		true );
 
-	wp_register_script( 'wpmslider', WPMTST_PUBLIC_URL . "js/lib/wpmslider/jquery.wpmslider{$min}.js", array( 'jquery-actual', 'imagesloaded' ), $plugin_version, true );
+	wp_register_script( 'wpmslider',
+		WPMTST_PUBLIC_URL . "js/lib/wpmslider/jquery.wpmslider{$min}.js",
+		array( 'jquery-actual', 'imagesloaded' ),
+		$plugin_version,
+		true );
 
-	wp_register_script( 'strongslider', WPMTST_PUBLIC_URL . 'js/jquery.strongslider.js', array( 'wpmslider', 'underscore' ), $plugin_version, true );
+	wp_register_script( 'strongslider',
+		WPMTST_PUBLIC_URL . 'js/jquery.strongslider.js',
+		array( 'wpmslider', 'underscore' ),
+		$plugin_version,
+		true );
 
 	if ( ! defined( 'MEGAMENU_VERSION' ) ) {
-		wp_register_script( 'wpmtst-slider', WPMTST_PUBLIC_URL . 'js/slider.js', array( 'strongslider' ), $plugin_version, true );
+		wp_register_script( 'wpmtst-slider',
+			WPMTST_PUBLIC_URL . 'js/slider.js',
+			array( 'strongslider' ),
+			$plugin_version,
+			true );
 	}
 
 }
@@ -109,7 +180,11 @@ function wpmtst_scripts_later() {
 	 * Custom slider handler for Max Mega Menu plugin.
 	 */
 	if ( defined( 'MEGAMENU_VERSION' ) ) {
-		wp_register_script( 'wpmtst-slider', WPMTST_PUBLIC_URL . 'js/slider-megamenu.js', array( 'strongslider' ), $plugin_version, true );
+		wp_register_script( 'wpmtst-slider',
+			WPMTST_PUBLIC_URL . 'js/slider-megamenu.js',
+			array( 'strongslider' ),
+			$plugin_version,
+			true );
 	}
 
 }
