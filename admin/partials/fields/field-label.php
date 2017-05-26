b5d00d800b848f648711e9f8e094b1ca46a53e2a
Chris Dillon <chrisdillon.us@gmail.com>
2017-05-26 17:26:00 -0400

    Formatting

--- admin/partials/fields/field-label.php
+++ admin/partials/fields/field-label.php
@@ -4,16 +4,15 @@
  */
 ?>
 <tr class="field-label-row">
-	<th><?php _ex( 'Label', 'noun', 'strong-testimonials' ); ?></th>
-	<td>
-		<input type="text" class="field-label"
-			   name="fields[<?php echo $key; ?>][label]"
-			   value="<?php echo wpmtst_htmlspecialchars( $field['label'] ); ?>">
-		<label>
-			<span class="help">
-				<input type="checkbox" name="fields[<?php echo $key; ?>][show_label]"
-					<?php checked( $field['show_label'], true ); ?>><?php _e( 'Show this label on the form.', 'strong-testimonials' ); ?>
-			</span>
-		</label>
-	</td>
+    <th><?php _ex( 'Label', 'noun', 'strong-testimonials' ); ?></th>
+    <td>
+        <input type="text" class="field-label"
+               name="fields[<?php echo $key; ?>][label]"
+               value="<?php echo wpmtst_htmlspecialchars( $field['label'] ); ?>">
+        <label>
+            <span class="help">
+                <input type="checkbox" name="fields[<?php echo $key; ?>][show_label]" <?php checked( $field['show_label'], true ); ?>><?php _e( 'Show this label on the form.', 'strong-testimonials' ); ?>
+            </span>
+        </label>
+    </td>
 </tr>
