22eca61b267601acf005342cc7622855e10f0733
Chris Dillon <chrisdillon.us@gmail.com>
2017-10-07 07:52:13 -0400

    Add UTM parameters to links in About page

--- admin/about/addons.php
+++ admin/about/addons.php
@@ -7,21 +7,25 @@
         <h3>Review Markup</h3>
         <p>Improve your search engine results by adding review markup to your testimonials. </p>
         <div class="actions">
-            <a href="https://strongplugins.com/plugins/strong-testimonials-review-markup" target="_blank">Learn more</a>
+            <a href="https://strongplugins.com/plugins/strong-testimonials-review-markup/?utm_source=plugin&utm_medium=about" target="_blank">Learn more</a>
+            &nbsp;&bull;&nbsp;
+            <a href="https://strongdemos.com/strong-testimonials-review-markup/?utm_source=plugin&utm_medium=about" target="_blank">Go demo</a>
         </div>
     </div>
     <div class="col sidebar-block sidebar-news addon multiple-forms">
         <h3>Multiple Forms</h3>
         <p>Need more forms for different products or services? No problem. Create unlimited forms.</p>
         <div class="actions">
-            <a href="https://strongplugins.com/plugins/strong-testimonials-multiple-forms" target="_blank">Learn more</a>
+            <a href="https://strongplugins.com/plugins/strong-testimonials-multiple-forms?utm_source=plugin&utm_medium=about" target="_blank">Learn more</a>
         </div>
     </div>
     <div class="col sidebar-block sidebar-news addon properties">
         <h3>Properties</h3>
         <p>Want to rebrand 'testimonials' as 'reviews'? Want to completely customize the permalink structure? Gain maximum control.</p>
         <div class="actions">
-            <a href="https://strongplugins.com/plugins/strong-testimonials-properties" target="_blank">Learn more</a>
+            <a href="https://strongplugins.com/plugins/strong-testimonials-properties/?utm_source=plugin&utm_medium=about" target="_blank">Learn more</a>
+            &nbsp;&bull;&nbsp;
+            <a href="https://strongdemos.com/strong-testimonials-properties/?utm_source=plugin&utm_medium=about" target="_blank">Go demo</a>
         </div>
     </div>
 </div>
