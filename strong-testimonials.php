67e1b6587dc3d98b5468606dc2aa320b6b208479
Chris Dillon <chrisdillon.us@gmail.com>
2017-10-30 05:25:14 -0400

    beta.1

--- strong-testimonials.php
+++ strong-testimonials.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://strongplugins.com/plugins/strong-testimonials/
  * Description: A full-featured plugin that works right out of the box for beginners and offers advanced features for pros.
  * Author: Chris Dillon
- * Version: 2.27.2.1
+ * Version: 2.28.0-beta.1
  *
  * Author URI: https://strongplugins.com/
  * Text Domain: strong-testimonials
@@ -162,7 +162,7 @@ final class Strong_Testimonials {
 	 * @return void
 	 */
 	private function setup_constants() {
-		defined( 'WPMTST_VERSION' ) || define( 'WPMTST_VERSION', '2.27.2' );
+		defined( 'WPMTST_VERSION' ) || define( 'WPMTST_VERSION', '2.28.0-beta.1' );
 
 		// plugin slug: `strong-testimonials` used by template search
 		defined( 'WPMTST_PLUGIN' ) || define( 'WPMTST_PLUGIN', plugin_basename( __FILE__ ) );
