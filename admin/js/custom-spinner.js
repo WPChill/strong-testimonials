bf473c8ae3089429ebf1960836425b0435965c44
Chris Dillon <chrisdillon.us@gmail.com>
2017-11-04 09:06:55 -0400

    Improve spinner

--- admin/js/custom-spinner.js
+++ admin/js/custom-spinner.js
@@ -14,37 +14,43 @@
     }
 
     var options = {
-      'containerClass': 'number-style',
+      'containerClass': 'number-style noselect',
       'minus': 'number-minus',
       'plus': 'number-plus',
       'containerTag': 'div',
       'btnTag': 'span'
     }
     options = $.extend(true, options, customOptions)
+
     var input = this
     input.wrap('<' + options.containerTag + ' class="' + options.containerClass + '">')
+
     var wrapper = input.parent()
     wrapper.prepend('<' + options.btnTag + ' class="' + options.minus + '"></' + options.btnTag + '>')
-    var minus = wrapper.find('.' + options.minus)
     wrapper.append('<' + options.btnTag + ' class="' + options.plus + '"></' + options.btnTag + '>')
+
+    var minus = wrapper.find('.' + options.minus)
     var plus = wrapper.find('.' + options.plus)
+
     var min = round(input.attr('min'),1)
     var max = round(input.attr('max'),1)
     var step = 1
     if(input.attr('step')){
       step = +round(input.attr('step'),1)
     }
+
     if (+input.val() <= +min) {
       minus.addClass('disabled')
     }
     if (+input.val() >= +max) {
       plus.addClass('disabled')
     }
+
     minus.click(function () {
       var input = $(this).parent().find('input')
       var value = +round(input.val(),1)
       if (+value > +min) {
-        input.val(+value - step)
+        input.val(round(+value - step,1))
         if (+input.val() === +min) {
           input.prev('.' + options.minus).addClass('disabled')
         }
@@ -52,14 +58,15 @@
           input.next('.' + options.plus).removeClass('disabled')
         }
       } else if (!min) {
-        input.val(+value - step)
+        input.val(round(+value - step,1))
       }
     })
+
     plus.click(function () {
       var input = $(this).parent().find('input')
       var value = +round(input.val(),2)
       if (+value < +max) {
-        input.val(+value + step)
+        input.val(round(+value + step,1))
         if (+input.val() === +max) {
           input.next('.' + options.plus).addClass('disabled')
         }
@@ -67,7 +74,7 @@
           input.prev('.' + options.minus).removeClass('disabled')
         }
       } else if (!max) {
-        input.val(+value + step)
+        input.val(round(+value + step,1))
       }
     })
   }
