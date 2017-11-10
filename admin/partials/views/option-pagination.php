7035ba119d05bc4dd08c0dac0896a0b8cfd79ac7
Chris Dillon <chrisdillon.us@gmail.com>
2017-11-10 16:32:01 -0500

    Apply coding standard

--- admin/partials/views/option-pagination.php
+++ admin/partials/views/option-pagination.php
@@ -5,12 +5,8 @@ $links .= '<a href="https://support.strongplugins.com/article/comparing-paginati
 ?>
 <?php /* translators: On the Views admin screen. */ ?>
 <th>
-    <input type="checkbox"
-           id="view-pagination"
-           class="if toggle"
-           name="view[data][pagination]"
-           value="1" <?php checked( $view['pagination'] ); ?>
-           class="checkbox">
+    <input class="if toggle checkbox" id="view-pagination" name="view[data][pagination]"
+           type="checkbox" value="1" <?php checked( $view['pagination'] ); ?>/>
     <label for="view-pagination">
 		<?php _e( 'Pagination', 'strong-testimonials' ); ?>
     </label>
@@ -20,7 +16,7 @@ $links .= '<a href="https://support.strongplugins.com/article/comparing-paginati
         <div class="row-inner">
             <div class="inline">
                 <label for="view-pagination_type">
-                    <select id="view-pagination_type" name="view[data][pagination_settings][type]" class="if selectper">
+                    <select class="if selectper" id="view-pagination_type" name="view[data][pagination_settings][type]">
                         <option value="simple" <?php selected( 'simple', $view['pagination_settings']['type'] ); ?>>
 							<?php _e( 'simple', 'strong-testimonials' ); ?>
                         </option>
@@ -51,9 +47,9 @@ $links .= '<a href="https://support.strongplugins.com/article/comparing-paginati
                 <label for="view-per_page">
 					<?php _ex( 'Per page', 'quantity', 'strong-testimonials' ); ?>
                 </label>
-                <input type="number" id="view-per_page" class="input-incremental"
-                       min="1" step="1" size="3"
+                <input class="input-incremental" id="view-per_page"
                        name="view[data][pagination_settings][per_page]"
+                       type="number" min="1" step="1"
                        value="<?php echo $view['pagination_settings']['per_page']; ?>"/>
             </div>
 
@@ -74,102 +70,93 @@ $links .= '<a href="https://support.strongplugins.com/article/comparing-paginati
                 </select>
             </div>
         </div>
-    </div>
 
-    <div class="row then then_not_simple then_standard" style="display: none;">
-        <div class="row-inner">
-            <div class="inline">
-                <label for="view-pagination-show_all">
-                    <select id="view-pagination-show_all"
-                            name="view[data][pagination_settings][show_all]"
-                            class="if select">
-                        <option value="on" <?php selected( $view['pagination_settings']['show_all'] ); ?>>
-							<?php _e( 'Show all page numbers', 'strong-testimonials' ); ?>
-                        </option>
-                        <option value="off" <?php selected( ! $view['pagination_settings']['show_all'] ); ?>
-                                class="trip">
-							<?php _e( 'Show condensed page numbers', 'strong-testimonials' ); ?>
-                        </option>
-                    </select>
-                </label>
-            </div>
-            <div class="inline then then_show_all" style="display: none;">
+        <div class="row then then_not_simple then_standard" style="display: none;">
+            <div class="row-inner">
                 <div class="inline">
-                    <label for="view-pagination-end_size">
-						<?php _ex( 'End size', 'quantity', 'strong-testimonials' ); ?>
+                    <label for="view-pagination-show_all">
+                        <select class="if select" id="view-pagination-show_all"
+                                name="view[data][pagination_settings][show_all]">
+                            <option value="on" <?php selected( $view['pagination_settings']['show_all'] ); ?>>
+                                <?php _e( 'Show all page numbers', 'strong-testimonials' ); ?>
+                            </option>
+                            <option value="off" <?php selected( ! $view['pagination_settings']['show_all'] ); ?>
+                                    class="trip">
+                                <?php _e( 'Show condensed page numbers', 'strong-testimonials' ); ?>
+                            </option>
+                        </select>
                     </label>
-                    <input type="number" id="view-pagination-end_size" class="input-incremental"
-                           min="1" step="1" size="3"
-                           name="view[data][pagination_settings][end_size]"
-                           value="<?php echo $view['pagination_settings']['end_size']; ?>"/>
                 </div>
-                <div class="inline">
-                    <label for="view-pagination-mid_size">
-						<?php _ex( 'Middle size', 'quantity', 'strong-testimonials' ); ?>
-                    </label>
-                    <input type="number" id="view-pagination-mid_size" class="input-incremental"
-                           min="1" step="1" size="3"
-                           name="view[data][pagination_settings][mid_size]"
-                           value="<?php echo $view['pagination_settings']['mid_size']; ?>"/>
+                <div class="inline then then_show_all" style="display: none;">
+                    <div class="inline">
+                        <label for="view-pagination-end_size">
+                            <?php _ex( 'End size', 'quantity', 'strong-testimonials' ); ?>
+                        </label>
+                        <input class="input-incremental" id="view-pagination-end_size"
+                               name="view[data][pagination_settings][end_size]"
+                               type="number" min="1" step="1"
+                               value="<?php echo $view['pagination_settings']['end_size']; ?>"/>
+                    </div>
+                    <div class="inline">
+                        <label for="view-pagination-mid_size">
+                            <?php _ex( 'Middle size', 'quantity', 'strong-testimonials' ); ?>
+                        </label>
+                        <input class="input-incremental" id="view-pagination-mid_size"
+                               name="view[data][pagination_settings][mid_size]"
+                               type="number" min="1" step="1"
+                               value="<?php echo $view['pagination_settings']['mid_size']; ?>"/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row then then_not_simple then_standard" style="display: none;">
-        <div class="row-inner">
-            <div class="inline inline-middle">
-                <input type="checkbox" id="view-pagination-prev_next" class="if toggle checkbox"
-                       name="view[data][pagination_settings][prev_next]" value="1"
-					<?php checked( $view['pagination_settings']['prev_next'] ); ?>>
-                <label for="view-pagination-prev_next">
-					<?php _e( 'Show previous/next links', 'strong-testimonials' ); ?>
-                </label>
-            </div>
-            <div class="then then_prev_next inline inline-middle">
-                <label for="view-pagination-prev_text">
-					<?php _e( 'Previous text', 'strong-testimonials' ); ?>
-                </label>
-                <input type="text"
-                       id="view-pagination-prev_text"
-                       class="code"
-                       name="view[data][pagination_settings][prev_text]"
-                       value="<?php echo htmlentities( $view['pagination_settings']['prev_text'] ); ?>">
-            </div>
-            <div class="then then_prev_next inline inline-middle">
-                <label for="view-pagination-next_text">
-					<?php _e( 'Next text', 'strong-testimonials' ); ?>
-                </label>
-                <input type="text"
-                       id="view-pagination-next_text"
-                       class="code"
-                       name="view[data][pagination_settings][next_text]"
-                       value="<?php echo htmlentities( $view['pagination_settings']['next_text'] ); ?>">
+        <div class="row then then_not_simple then_standard" style="display: none;">
+            <div class="row-inner">
+                <div class="inline inline-middle">
+                    <input class="if toggle checkbox" id="view-pagination-prev_next"
+                           name="view[data][pagination_settings][prev_next]"
+                           type="checkbox" value="1" <?php checked( $view['pagination_settings']['prev_next'] ); ?>>
+                    <label for="view-pagination-prev_next">
+                        <?php _e( 'Show previous/next links', 'strong-testimonials' ); ?>
+                    </label>
+                </div>
+                <div class="then then_prev_next inline inline-middle">
+                    <label for="view-pagination-prev_text">
+                        <?php _e( 'Previous text', 'strong-testimonials' ); ?>
+                    </label>
+                    <input class="code" id="view-pagination-prev_text"
+                           name="view[data][pagination_settings][prev_text]"
+                           type="text" value="<?php echo htmlentities( $view['pagination_settings']['prev_text'] ); ?>">
+                </div>
+                <div class="then then_prev_next inline inline-middle">
+                    <label for="view-pagination-next_text">
+                        <?php _e( 'Next text', 'strong-testimonials' ); ?>
+                    </label>
+                    <input class="code" id="view-pagination-next_text"
+                           name="view[data][pagination_settings][next_text]"
+                           type="text" value="<?php echo htmlentities( $view['pagination_settings']['next_text'] ); ?>">
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="row then then_not_simple then_standard" style="display: none;">
-        <div class="row-inner">
-            <div class="inline">
-                <label for="view-pagination-before_page_number">
-					<?php _e( 'Before page number', 'strong-testimonials' ); ?>
-                </label>
-                <input type="text"
-                       id="view-pagination-before_page_number"
-                       class="small-text"
-                       name="view[data][pagination_settings][before_page_number]"
-                       value="<?php echo $view['pagination_settings']['before_page_number']; ?>">
-            </div>
-            <div class="inline">
-                <label for="view-pagination-after_page_number">
-					<?php _e( 'After page number', 'strong-testimonials' ); ?>
-                </label>
-                <input type="text"
-                       id="view-pagination-after_page_number"
-                       class="small-text"
-                       name="view[data][pagination_settings][after_page_number]"
-                       value="<?php echo $view['pagination_settings']['after_page_number']; ?>">
+        <div class="row then then_not_simple then_standard" style="display: none;">
+            <div class="row-inner">
+                <div class="inline">
+                    <label for="view-pagination-before_page_number">
+                        <?php _e( 'Before page number', 'strong-testimonials' ); ?>
+                    </label>
+                    <input class="small-text" id="view-pagination-before_page_number"
+                           name="view[data][pagination_settings][before_page_number]"
+                           type="text" value="<?php echo $view['pagination_settings']['before_page_number']; ?>">
+                </div>
+                <div class="inline">
+                    <label for="view-pagination-after_page_number">
+                        <?php _e( 'After page number', 'strong-testimonials' ); ?>
+                    </label>
+                    <input class="small-text" id="view-pagination-after_page_number"
+                           name="view[data][pagination_settings][after_page_number]"
+                           type="text" value="<?php echo $view['pagination_settings']['after_page_number']; ?>">
+                </div>
             </div>
         </div>
     </div>
