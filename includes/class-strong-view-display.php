838fda103e6496cd7dbf04048e7ac6d22f29ac4f
Chris Dillon <chrisdillon.us@gmail.com>
2017-11-16 10:02:19 -0500

    Standard pagination forces quantity:all

--- includes/class-strong-view-display.php
+++ includes/class-strong-view-display.php
@@ -168,6 +168,8 @@ class Strong_View_Display extends Strong_View {
 		);
 
 		if ( $this->atts['pagination'] && 'standard' == $this->atts['pagination_settings']['type'] ) {
+			// Limit is not compatible with standard pagination.
+			$this->atts['all'] = true;
 			$args['posts_per_page'] = $this->atts['pagination_settings']['per_page'];
 			$args['paged']          = wpmtst_get_paged();
 		}
@@ -231,10 +233,15 @@ class Strong_View_Display extends Strong_View {
 		 * @since 1.16.1
 		 */
 		if ( ! $this->atts['all'] && $this->atts['count'] > 0 ) {
-			$count              = min( $this->atts['count'], count( $query->posts ) );
-			$query->posts       = array_slice( $query->posts, 0, $count );
-			$query->post_count  = $count;
-			$query->found_posts = $count;
+			if ( $this->atts['pagination'] ) {
+				if ( 'simple' == $this->atts['pagination_settings']['type'] ) {
+					$count = min( $this->atts['count'], count( $query->posts ) );
+				}
+			}
+			$query->posts         = array_slice( $query->posts, 0, $count );
+			$query->post_count    = $count;
+			$query->found_posts   = $count;
+			$query->max_num_pages = ceil( $count / $this->atts['pagination_settings']['per_page'] );
 		}
 
 		$this->post_count  = $query->post_count;
