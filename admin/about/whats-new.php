a3e7b561f63cfa4dda37ab40ce63f6fc2e5b9670
Chris Dillon <chrisdillon.us@gmail.com>
2018-01-02 15:01:22 -0500

    Update About page

--- admin/about/whats-new.php
+++ admin/about/whats-new.php
@@ -1,5 +1,18 @@
 <h2>Now Even Stronger</h2>
 
+<p class="lead-description">Version 2.29 improves Captcha options and fixes two bugs.</p>
+<div class="feature-section one-col">
+    <div class="col">
+        <p>NEW: Integration with Google Captcha by BestWebSoft. This allows use of either Invisible reCaptcha and reCatpcha V2, both excellent choices for modern spam warfare.
+        <p>REMOVED: Integration with the plugin Captcha due to security issues.</p>
+        <p>REMOVED: Integration with the plugin Advanced noCaptcha reCaptcha due to compatibility issues and inadequate maintenance.</p>
+        <p>FIX: The notification email now displays a category name properly.</p>
+        <p>IMPROVED: The notification settings UI was improved to make it easier to add your custom fields to the notification email.</p>
+        <p>NEW: A new filter on the "Read more" page for total control over that link.</p>
+        <p>FIX: A conflict with Cherry Slider that made the slideshow invisible.</p>
+    </div>
+</div>
+
 <p class="lead-description">Version 2.28 improves compatibility and adds some new features.</p>
 <div class="feature-section one-col">
     <div class="col">
