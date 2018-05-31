977557ce577c52fa94ec3900bb40bbf6c3296d46
Chris Dillon <chrisdillon.us@gmail.com>
2018-05-31 15:33:30 -0400

    Use esc_attr

--- admin/partials/views/group-fields.php
+++ admin/partials/views/group-fields.php
@@ -1,20 +1,75 @@
-<?php /* translators: On the Views admin screen. */ ?>
-<div class="then then_display then_not_form then_slideshow then_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'fields' ); ?>" style="display: none;">
+<?php
+$then_classes = array(
+	'then',
+	'then_display',
+	'then_not_form',
+	'then_slideshow',
+	'then_single_template',
+	apply_filters( 'wpmtst_view_section', '', 'fields' ),
+);
+?>
+<div class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 	<h3>
+		<?php /* translators: On the Views admin screen. */ ?>
 		<?php _e( 'Fields', 'strong-testimonials' ); ?>
 	</h3>
 	<table class="form-table multiple group-show">
-		<tr class="then then_display then_not_form then_slideshow then_not_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'title' ); ?>" style="display: none;">
+
+		<?php
+		$then_classes = array(
+			'then',
+			'then_display',
+			'then_not_form',
+			'then_slideshow',
+			'then_not_single_template',
+			apply_filters( 'wpmtst_view_section', '', 'title' ),
+		);
+		?>
+		<tr class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 			<?php include( 'option-title.php' ); ?>
 		</tr>
-		<tr class="then then_display then_not_form then_slideshow then_not_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'thumbnail' ); ?>" style="display: none;">
+
+		<?php
+		$then_classes = array(
+			'then',
+			'then_display',
+			'then_not_form',
+			'then_slideshow',
+			'then_not_single_template',
+			apply_filters( 'wpmtst_view_section', '', 'thumbnail' ),
+		);
+		?>
+		<tr class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 			<?php include( 'option-thumbnail.php' ); ?>
 		</tr>
-		<tr class="then then_display then_not_form then_slideshow then_not_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'content' ); ?>" style="display: none;">
+
+		<?php
+		$then_classes = array(
+			'then',
+			'then_display',
+			'then_not_form',
+			'then_slideshow',
+			'then_not_single_template',
+			apply_filters( 'wpmtst_view_section', '', 'content' ),
+		);
+		?>
+		<tr class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 			<?php include( 'option-content.php' ); ?>
 		</tr>
-		<tr class="then then_display then_not_form then_slideshow then_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'client-section' ); ?>" style="display: none;">
+
+		<?php
+		$then_classes = array(
+			'then',
+			'then_display',
+			'then_not_form',
+			'then_slideshow',
+			'then_single_template',
+			apply_filters( 'wpmtst_view_section', '', 'client-section' ),
+		);
+		?>
+		<tr class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 			<?php include( 'option-client-section.php' ); ?>
 		</tr>
+
 	</table>
 </div>
--- admin/partials/views/group-form.php
+++ admin/partials/views/group-form.php
@@ -1,6 +1,16 @@
-<?php /* translators: On the Views admin screen. */ ?>
-<div class="then then_not_display then_not_slideshow then_form then_not_single_template <?php echo apply_filters( 'wpmtst_view_section', '', 'form' ); ?>" style="display: none;">
+<?php
+$then_classes = array(
+	'then',
+	'then_not_display',
+	'then_not_slideshow',
+	'then_form',
+	'then_not_single_template',
+	apply_filters( 'wpmtst_view_section', '', 'form' ),
+);
+?>
+<div class="<?php echo esc_attr( join( array_filter( $then_classes ), ' ' ) ); ?>" style="display: none;">
 	<h3>
+		<?php /* translators: On the Views admin screen. */ ?>
 		<?php _e( 'Actions', 'strong-testimonials' ); ?>
 	</h3>
 	<table class="form-table multiple group-select">
