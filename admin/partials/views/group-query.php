8cd30984fc897404ffd1934340d54ac5dc0ad46d
Chris Dillon <chrisdillon.us@gmail.com>
2018-05-31 15:56:56 -0400

    i18n

--- admin/partials/views/group-query.php
+++ admin/partials/views/group-query.php
@@ -15,9 +15,16 @@ $then_classes = array(
 	</h3>
 	<table class="form-table multiple group-select">
         <tr class="subheading">
-            <td>Option</td>
-            <td>Setting</td>
-            <td class="divider" colspan="2">or Shortcode Attribute <span class="help-links"><span class="description"><a href="#tab-panel-wpmtst-help-shortcode" class="open-help-tab"><?php _e( 'Help' ); ?></a></span></span></td>
+            <td><?php _e( 'Option', 'strong-testimonials' ); ?></td>
+            <td><?php _e( 'Setting', 'strong-testimonials' ); ?></td>
+            <td class="divider" colspan="2">
+				<?php _e( 'or Shortcode Attribute', 'strong-testimonials' ); ?>
+				<span class="help-links">
+					<span class="description">
+						<a href="#tab-panel-wpmtst-help-shortcode" class="open-help-tab"><?php _e( 'Help' ); ?></a>
+					</span>
+				</span>
+			</td>
             <td>Example</td>
         </tr>
 		<tr class="then then_display then_not_slideshow then_not_form" style="display: none;">
