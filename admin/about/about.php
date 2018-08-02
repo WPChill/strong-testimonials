450f0e82d6a273fc1327f1927e23cf7b12690d95
Chris Dillon <chrisdillon.us@gmail.com>
2018-08-02 14:59:51 -0400

    Update About pages

--- admin/about/about.php
+++ admin/about/about.php
@@ -1,11 +1,11 @@
 <h2><?php _e( 'Flexible Features. Strong Support.', 'strong-testimonials' ); ?></h2>
 <?php /* translators: %s is a formatted number */ ?>
-<p class="lead-description"><?php printf( __( 'The highest-rated free testimonials plugin trusted by more than %s people keeps getting better.', 'strong-testimonials' ), number_format_i18n( 50000 ) ); ?></p>
+<p class="lead-description"><?php printf( __( 'The highest-rated free testimonials plugin trusted by more than %s people.', 'strong-testimonials' ), number_format_i18n( 60000 ) ); ?></p>
 <div class="feature-section one-col">
     <div class="col">
-        <p><?php _e( 'Your site is unique, right? So are your testimonials. This plugin is designed from the ground up to simplify the process of customizing your testimonials or reviews.', 'strong-testimonials' ); ?></p>
-        <p><?php _e( '<strong>Custom fields</strong> are deeply integrated into Strong Testimonials so it\'s simple to configure the fields on your testimonial submission form, update those fields when adding or editing testimonials, and select which fields to display on your testimonials. The plugin even includes a default set of custom fields so you can get started quickly.', 'strong-testimonials' ); ?></p>
+        <p><?php _e( 'Designed from the ground up to simplify the process of customizing your testimonials or reviews.', 'strong-testimonials' ); ?></p>
+        <p><?php _e( '<strong>Custom fields</strong> are deeply integrated into Strong Testimonials. It even includes a default set of custom fields so you can get started quickly.', 'strong-testimonials' ); ?></p>
         <p><?php _e( 'Forget about complicated shortcodes. Use <strong>views</strong> to construct unlimited testimonial displays including grids and slideshows with features like excerpts and pagination in a variety of templates.', 'strong-testimonials' ); ?></p>
-        <p><?php _e( 'Power up with plugins like Popup Maker or Google Captcha. For developers, actions and filters allow further customization.', 'strong-testimonials' ); ?></p>
+        <p><?php _e( 'Integrate with plugins like Popup Maker or Google Captcha. For developers, actions and filters allow further customization.', 'strong-testimonials' ); ?></p>
     </div>
 </div>
