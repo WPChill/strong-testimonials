5a1ae8250ca2a0ac287bc03a3b1770c4fe060962
Chris Dillon <chrisdillon.us@gmail.com>
2018-12-31 09:38:50 -0500

    Improve  UI
    Show ellipsis at end of animation.

--- public/js/lib/readmore/readmore.js
+++ public/js/lib/readmore/readmore.js
@@ -22,6 +22,8 @@
       event.target.setAttribute('hidden', 'true');
       // Show read-more link
       event.target.parentElement.querySelector('.readmore').style.display = 'inline';
+      // Show ellipsis
+      event.target.parentElement.querySelector('.ellipsis').style.display = 'inline';
     }
   };
 
@@ -41,9 +43,13 @@
 
     forEach(toggleButtons, function (index, toggleButton) {
 
-      toggleButton.addEventListener('click', function (event) {
+      toggleButton.addEventListener('click', function () {
 
         var fullTextWrapper = this.parentElement.querySelector('.readmore-content');
+        if (!fullTextWrapper) {
+          return;
+        }
+
         var ellipsis = this.parentElement.querySelector('.ellipsis');
         var toggleButtonText = this.querySelector('.readmore-text');
 
@@ -75,8 +81,6 @@
           toggleButton.setAttribute('aria-expanded', false);
           // change link text
           toggleButtonText.innerText = 'Show More';
-          // Show ellipsis
-          ellipsis.style.display = 'inline';
 
           // 2. animate it
           fullTextWrapper.classList.add('fadeOutUp');
