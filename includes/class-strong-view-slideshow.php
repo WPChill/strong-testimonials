364f88ca5a08277f2a3e98ceead154aeca62483a
Chris Dillon <chrisdillon.us@gmail.com>
2018-12-31 05:54:30 -0500

    Add filter for slider debug

--- includes/class-strong-view-slideshow.php
+++ includes/class-strong-view-slideshow.php
@@ -396,7 +396,7 @@ class Strong_View_Slideshow extends Strong_View_Display {
 			'autoControls'        => 0,
 			'pager'               => 0,
 			'slideCount'          => $this->post_count,
-			'debug'               => defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG,
+			'debug'               => defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && apply_filters( 'debug_strong_slider', true ),
 			'compat'              => $compat,
 			'touchEnabled'        => $options['touch_enabled'],
 			'type'                => $this->atts['slideshow_settings']['type'],
