ef26821be4ea685182066c6e2113811cdc2e20b4
cristianraiber <cristian@machothemes.com>
2019-02-01 13:04:14 +0200

    minor fix 2

--- admin/partials/views/template-input.php
+++ admin/partials/views/template-input.php
@@ -1,6 +1,6 @@
 <div>
-	<input type="radio" id="template-<?php esc_attr( $key ); ?>" name="view[data][<?php esc_attr( $current_mode ); ?>]" value="<?php esc_attr( $key ); ?>" <?php checked( $key, $view['template'] ); ?>>
-	<label for="template-<?php esc_attr( $key ); ?>">
+	<input type="radio" id="template-<?php echo esc_attr( $key ); ?>" name="view[data][<?php echo esc_attr( $current_mode ); ?>]" value="<?php echo esc_attr( $key ); ?>" <?php checked( $key, $view['template'] ); ?>>
+	<label for="template-<?php echo esc_attr( $key ); ?>">
 		<?php echo wp_kses_post( $template['config']['name'] ); ?>
 	</label>
 </div>
