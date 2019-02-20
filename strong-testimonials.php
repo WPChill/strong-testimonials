0cea5aec1a0354988a392a5fb570f057dcd61435
cristianraiber <cristian@machothemes.com>
2019-02-20 12:34:00 +0200

    ability to export testimonials with featured images included - close #27

--- strong-testimonials.php
+++ strong-testimonials.php
@@ -279,6 +279,7 @@ final class Strong_Testimonials {
 			require_once WPMTST_ADMIN . 'class-strong-testimonials-admin-list.php';
 			require_once WPMTST_ADMIN . 'class-strong-testimonials-admin-category-list.php';
 			require_once WPMTST_ADMIN . 'class-strong-testimonials-post-editor.php';
+			require_once WPMTST_ADMIN . 'class-strong-testimonials-exporter.php';
 
 			require_once WPMTST_ADMIN . 'admin.php';
 			require_once WPMTST_ADMIN . 'admin-notices.php';
