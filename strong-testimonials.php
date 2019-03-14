8dfd09822d8dbfd4e12730a60b69f5fac417d12b
unknown <cristi.ecmecian@yahoo.com>
2019-03-14 16:53:20 +0200

    version bump

--- strong-testimonials.php
+++ strong-testimonials.php
@@ -5,7 +5,7 @@
  * Description: Collect and display your testimonials or reviews.
  * Author: MachoThemes
  * Author URI: https://www.machothemes.com/
- * Version: 2.35
+ * Version: 2.36
  * Text Domain: strong-testimonials
  * Domain Path: /languages
  * Requires: 4.6 or higher
@@ -44,7 +44,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPMTST_VERSION', '2.35' );
+define( 'WPMTST_VERSION', '2.36' );
 define( 'WPMTST_PLUGIN', plugin_basename( __FILE__ ) ); // strong-testimonials/strong-testimonials.php
 define( 'WPMTST', dirname( WPMTST_PLUGIN ) );           // strong-testimonials
 define( 'STRONGPLUGINS_STORE_URL', '#' );
