679936b3b6c93b8a5e8db68df6e6d0eb15c3e633
unknown <cristi.ecmecian@yahoo.com>
2019-05-08 14:27:23 +0300

    get addons from store server

--- admin/class-strong-testimonials-addons.php
+++ admin/class-strong-testimonials-addons.php
@@ -6,63 +6,38 @@
  */
 class Strong_Testimonials_Addons {
 
-	private $addons;
-	public $upgrade_url = 'https://strongtestimonials.com/pricing/?utm_source=st-lite&utm_campaign=upsell';
+	private $addons = array();
 
 	public function __construct() {
-		$this->addons = $this->check_for_addons();
-		$this->add_actions();
+		add_filter( 'wpmtst_submenu_pages', array( $this, 'add_submenu' ) );
 	}
 
 	private function check_for_addons() {
 
-		$addons = array(
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Custom Fields', 'strong-testimonials' ),
-				'description' => esc_html__( 'Enhance your testimonials and submission forms with the Custom Fields extension to both collect and display additional information.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-custom-fields',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Country Selector', 'strong-testimonials' ),
-				'description' => esc_html__( 'Allow customers to select their country when submitting testimonials.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-country-selector',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Assignment', 'strong-testimonials' ),
-				'description' => esc_html__( 'Assign testimonials to custom post types.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-assignment',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Multiple Forms', 'strong-testimonials' ),
-				'description' => esc_html__( 'Easily collect testimonials from customers by creating and customizing multiple forms at once.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-multiple-forms',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Review Markup', 'strong-testimonials' ),
-				'description' => esc_html__( 'SEO-friendly Testimonials. Take full advantage of your testimonials with our Schema.org Markup extension.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-review-markup',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Custom Properties', 'strong-testimonials' ),
-				'description' => esc_html__( 'Change properties of the testimonial post type: labels, permalink structure, admin options and post editor features.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-properties',
-			),
-			array(
-				'image'       => '',
-				'name'        => esc_html__( 'Strong Testimonials Advanced Views', 'strong-testimonials' ),
-				'description' => esc_html__( 'Customize testimonials beyond star ratings with the Advanced Views extension.', 'strong-testimonials' ),
-				'slug'        => 'strong-testimonials-advanced-views',
-			),
-		);
+		if ( false !== ( $data = get_transient( 'strong_testimonials_all_extensions' ) ) ) {
+			return $data;
+		}
+
+		$addons = array();
+
+		$url = apply_filters( 'strong_testimonials_addon_server_url', WPMTST_STORE_URL . '/wp-json/mt/v1/get-all-addons' );
+
+		// Get data from the remote URL.
+		$response = wp_remote_get( $url );
+
+		if ( ! is_wp_error( $response ) ) {
+
+			// Decode the data that we got.
+			$data = json_decode( wp_remote_retrieve_body( $response ), true );
+
+			if ( ! empty( $data ) && is_array( $data ) ) {
+				$addons = $data;
+				// Store the data for a week.
+				set_transient( 'strong_testimonials_all_extensions', $data, 7 * DAY_IN_SECONDS );
+			}
+		}
 
 		return apply_filters( 'wpmtst_addons', $addons );
-
 	}
 
 	public function render_addons() {
@@ -81,19 +56,12 @@ class Strong_Testimonials_Addons {
 				echo '</div>';
 				echo '</div>';
 				echo '<div class="wpmtst-addon-actions">';
-				echo apply_filters( 'wpmtst_addon_button_action', '<a href="' . esc_url( $this->upgrade_url ) . '&utm_medium=' . esc_attr( $addon['slug'] ) . '" target="_blank" class="button primary-button">' . esc_html__( 'Upgrade to PRO', 'strong-testimonials' ) . '</a>', $addon );
+				echo apply_filters( 'wpmtst_addon_button_action', '<a href="' . esc_url( WPMTST_STORE_UPGRADE_URL . '?utm_source=st-lite&utm_campaign=upsell&utm_medium=' . esc_attr( $addon['slug'] ) ) . '" target="_blank" class="button primary-button">' . esc_html__( 'Upgrade to PRO', 'strong-testimonials' ) . '</a>', $addon );
 				echo '</div>';
 				echo '</div>';
 			}
 		}
 
-	}
-
-	/**
-	 * Add actions and filters.
-	 */
-	public function add_actions() {
-		add_filter( 'wpmtst_submenu_pages', array( $this, 'add_submenu' ) );
 	}
 
 	/**
@@ -128,8 +96,12 @@ class Strong_Testimonials_Addons {
 	 * Print the Addons page.
 	 */
 	public function addons_page() {
+
+		$this->addons = $this->check_for_addons();
 		?>
-		<div class="wrap about-wrap">
+
+		<div class="wrap">
+			<h1 style="margin-bottom: 20px"><?php esc_html_e( 'Extensions' ); ?></h1>
 			<div class="wpmtst-addons-container">
 				<?php $this->render_addons(); ?>
 			</div>
