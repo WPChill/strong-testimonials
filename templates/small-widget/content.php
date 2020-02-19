5273055cf6b68b83bc1e8b96b21da5b660df07f5
razvanaldea89 <razvan.aldea89@yahoo.ro>
2020-02-19 13:22:24 +0200

    filters update

--- templates/small-widget/content.php
+++ templates/small-widget/content.php
@@ -4,27 +4,20 @@
  * Description: The default widget template.
  * Force: view-layout-normal
  */
-?>
-<?php do_action( 'wpmtst_before_view' ); 
-$view = new Strong_View_Slideshow( $atts );
 
-if( class_exists( 'Strong_Testimonials_Pro') && 1 == $view->atts['st_pro_filters'] && 'display' == $view->atts['mode'] ) {
-	$class_check = '';
-}else {
-	$class_check = "wpmtst_post_class";
-}
+do_action( 'wpmtst_before_view' );
 ?>
 
 <div class="strong-view strong-widget <?php wpmtst_container_class(); ?>"<?php wpmtst_container_data(); ?>>
 	<?php do_action( 'wpmtst_view_header' ); ?>
 
 	<div class="strong-content <?php wpmtst_content_class(); ?>">
-	<?php do_action( 'wpmtst_before_content', $view ); ?>
+	<?php do_action( 'wpmtst_before_content', $atts ); ?>
 
 		<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-		<div class="<?php ('wpmtst_post_class' == $class_check) ?  $class_check() : $class_check ?>">
+		<div class="<?php wpmtst_post_class($atts); ?>">
 
-			<?php do_action('wpmtst_before_testimonial_inner', $view, $post) ?>
+			<?php do_action('wpmtst_before_testimonial_inner', $atts, $post) ?>
 			<div class="testimonial-inner">
 				<?php do_action( 'wpmtst_before_testimonial' ); ?>
 
@@ -41,13 +34,13 @@ if( class_exists( 'Strong_Testimonials_Pro') && 1 == $view->atts['st_pro_filters
 
 				<div class="clear"></div>
 
-				<?php do_action( 'wpmtst_after_testimonial' ,$view); ?>
+				<?php do_action( 'wpmtst_after_testimonial' ,$atts); ?>
 			</div>
 
 		</div>
 		<?php endwhile; ?>
 
-		<?php do_action( 'wpmtst_after_testimonial' ,$view); ?>
+		<?php do_action( 'wpmtst_after_testimonial' ,$atts); ?>
 	</div>
 
 	<?php do_action( 'wpmtst_view_footer' ); ?>
--- templates/unstyled/content.php
+++ templates/unstyled/content.php
@@ -3,27 +3,20 @@
  * Template Name: Unstyled
  * Description: A completely unstyled template for CSS experts.
  */
-?>
-<?php do_action( 'wpmtst_before_view' ); 
-$view = new Strong_View_Slideshow( $atts );
 
-if( class_exists( 'Strong_Testimonials_Pro') && 1 == $view->atts['st_pro_filters'] && 'display' == $view->atts['mode'] ) {
-	$class_check = '';
-}else {
-	$class_check = "wpmtst_post_class";
-}
+do_action( 'wpmtst_before_view' );
 ?>
 
 <div class="strong-view <?php wpmtst_container_class(); ?>"<?php wpmtst_container_data(); ?>>
 	<?php do_action( 'wpmtst_view_header' ); ?>
 
 	<div class="strong-content <?php wpmtst_content_class(); ?>">
-		<?php do_action( 'wpmtst_before_content', $view ); ?>
+		<?php do_action( 'wpmtst_before_content', $atts ); ?>
 
 		<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-			<div class="<?php ('wpmtst_post_class' == $class_check) ?  $class_check() : $class_check ?>">
+			<div class="<?php wpmtst_post_class($atts); ?>">
 
-				<?php do_action('wpmtst_before_testimonial_inner', $view, $post) ?>
+				<?php do_action('wpmtst_before_testimonial_inner', $atts, $post) ?>
 				<div class="testimonial-inner">
 					<?php do_action( 'wpmtst_before_testimonial' ); ?>
 
@@ -40,13 +33,13 @@ if( class_exists( 'Strong_Testimonials_Pro') && 1 == $view->atts['st_pro_filters
 
 					<div class="clear"></div>
 
-					<?php do_action( 'wpmtst_after_testimonial' ,$view); ?>
+					<?php do_action( 'wpmtst_after_testimonial' ,$atts); ?>
 				</div>
 
 			</div>
 		<?php endwhile; ?>
 
-		<?php do_action( 'wpmtst_after_content',$view ) ?>
+		<?php do_action( 'wpmtst_after_content',$atts ) ?>
 	</div>
 
 	<?php do_action( 'wpmtst_view_footer' ); ?>
