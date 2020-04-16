1bed8466ae34001ec189ec7786e8b471fa2a8a92
AndylukAK <alexandru.tirim@yahoo.com>
2020-04-16 16:27:22 +0300

    Version Bump
    - add old templeate classes

--- includes/class-strong-view.php
+++ includes/class-strong-view.php
@@ -365,7 +365,7 @@ class Strong_View {
 		$class = str_replace( ':content', '', $template_name );
 		$class = str_replace( ':', '-', $class );
 		$class = str_replace( '-form-form', '-form', $class );
-		$class = 'wpmtst-' . $class . ' strong-' . $class;
+		$class = $class . ' wpmtst-' . $class ;
 		$class_list = array( $class );
 
 		$template_object = WPMST()->templates->get_template_by_name( $template_name );
