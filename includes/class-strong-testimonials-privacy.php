c075d047959213c891c36b531c0b2dbe9c2452de
Cristina <cristinna.iulia@gmail.com>
2020-05-21 12:26:22 +0300

    #106 added GDPR to testimonials

--- includes/class-strong-testimonials-privacy.php
+++ includes/class-strong-testimonials-privacy.php
@@ -1,5 +1,4 @@
 <?php
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -21,6 +20,7 @@ class Strong_Testimonials_Privacy {
 	public function add_hooks() {
 		add_filter( 'wp_privacy_personal_data_exporters', array( $this, 'register_exporter' ) );
 		add_filter( 'wp_privacy_personal_data_erasers', array( $this, 'register_eraser' ) );
+                add_action( 'admin_init', array($this, 'wpmtst_register_privacy_policy_template') );
 	}
 
 	/**
@@ -66,10 +66,20 @@ class Strong_Testimonials_Privacy {
 			foreach ( $post_meta as $key => $value ) {
 				$found = array_search( $email_address, $value );
 				if ( false !== $found ) {
-					$data[] = array(
-						'name'  => $key,
-						'value' => $value[ $found ],
-					);
+					$data = array(
+                                                array(
+                                                    'name'  => 'Email',
+                                                    'value' => $value[ $found ],
+                                                ),
+                                                array(
+                                                    'name'  => 'Client Name',
+                                                    'value' => $post_meta['client_name'][0],
+                                                ),
+                                                array(
+                                                    'name'  => 'Company Name',
+                                                    'value' => $post_meta['company_name'][0],
+                                                ),
+                                            );
 				}
 			}
 
@@ -117,14 +127,15 @@ class Strong_Testimonials_Privacy {
 		$testimonials = get_posts( $args );
 
 		foreach ( (array) $testimonials as $testimonial ) {
-			$post_meta = get_post_meta( $testimonial->ID );
+                    $post_meta = get_post_meta( $testimonial->ID );
 
-			foreach ( $post_meta as $key => $value ) {
-				if ( in_array( $email_address, $value ) ) {
-					delete_post_meta( $testimonial->ID, $key, $email_address );
-					$items_removed = true;
-				}
-			}
+                    foreach ( $post_meta as $key => $value ) {
+                            if ( in_array( $email_address, $value ) ) {
+                                    //delete_post_meta( $testimonial->ID, $key, $email_address );
+                                    wp_delete_post($testimonial->ID);
+                                    $items_removed = true;
+                            }
+                    }
 
 		}
 
@@ -167,5 +178,22 @@ class Strong_Testimonials_Privacy {
 
 		return $erasers;
 	}
+        
+        function wpmtst_register_privacy_policy_template() {
+
+            if ( ! function_exists( 'wp_add_privacy_policy_content' ) ) {
+                    return;
+            }
+
+            $content = wp_kses_post( apply_filters( 'wpmtst_privacy_policy_content', __( '
+                We collect information about you when you fill out a form for a testimonial for one of our product or service. This information include your full name, email address, your photo and company name and website.           
+                Handling this data also allows us to:
+                - Send you an email when your testimonial was received and approved.
+                - Send you important account/product/service information.
+                - Set up and administer your account, provide technical and/or customer support, and to verify your identity.
+            ', 'strong-testimonial-downloads' ) ) );
+
+            wp_add_privacy_policy_content( 'Strong Testimonial Privacy Policy', wpautop( $content ) );
+    }
 
 }
