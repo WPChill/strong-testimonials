4690336f52d5a27b99d3778299fa4ae7275031bc
Cristina <cristinna.iulia@gmail.com>
2020-07-14 12:05:28 +0300

    fixed navigation and pagination bug

--- public/js/lib/strongslider/jquery-strongslider.js
+++ public/js/lib/strongslider/jquery-strongslider.js
@@ -30,7 +30,7 @@
 	  mode: 'horizontal',
 	  slideSelector: 'div.t-slide',
 	  infiniteLoop: true,
-	  hideControlOnEnd: false,
+	  hideControlOnEnd: true,
 	  speed: 500,
 	  easing: null,
 	  slideMargin: 10,
