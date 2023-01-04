74b2fede404533d4fa671d3629abf166c627a220
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-01-04 15:25:46 +0200

    [ #309 ] - proper banner; font inclusion;

--- admin/class-strong-testimonials-onboarding.php
+++ admin/class-strong-testimonials-onboarding.php
@@ -17,19 +17,19 @@ class WPMTST_Onboarding extends WP_Posts_List_Table
 		<div class="wpmtst-onboarding-wrapper">
 
 			<div class="wpmtst-onboarding-title">
-				<img src="<?php echo esc_url( WPMTST_ADMIN_URL ) .'img/onboarding/WPChill Onboarding Wave.png';?>" class="wpmtst-onboarding-title-icon" /> <span><?php esc_html_e( 'Hi, there!', 'strong-testimonials' ); ?></span>
+				<img src="<?php echo esc_url( WPMTST_ADMIN_URL ) .'img/onboarding/WPChill-Onboarding-Wave.png';?>" class="wpmtst-onboarding-title-icon" /> <span><?php esc_html_e( 'Hi, there!', 'strong-testimonials' ); ?></span>
 			</div>
 			<div class="wpmtst-onboarding-text-wrap">
-				<p><?php esc_html_e( 'With its intuitive interface and wide range of features, wpmtst makes it easy to create a professional-looking gallery that showcases your photos and videos in the best light. Whether you want to share your work with friends, family, or potential clients, wpmtst can help you create a gallery that impresses and engages.', 'strong-testimonials' ); ?></p>
+				<p><?php esc_html_e( 'In just a few steps, you will be collecting and publishing your testimonials or reviews. Beginners and pros alike will appreciate the wealth of flexible features refined over 4 years from user feedback and requests.', 'strong-testimonials' ); ?></p>
 			</div>
 			<div class="wpmtst-onboarding-banner-wrap">
-				<img src="<?php echo esc_url( WPMTST_ADMIN_URL ) .'img/onboarding/WPChill onboarding Banner.png';?>" class="wpmtst-onboarding-banner" />
+				<img src="<?php echo esc_url( WPMTST_ADMIN_URL ) .'img/onboarding/st-banner.png';?>" class="wpmtst-onboarding-banner" />
 			</div>
 			<div class="wpmtst-onboarding-button-wrap">
 				<a href="<?php echo esc_url( $new_gal_url ); ?>" class="wpmtst-onboarding-button"><?php esc_html_e( 'Create your first testimonial', 'strong-testimonials' ); ?></a>
 			</div>
 			<div class="wpmtst-onboarding-doc-wrap">
-				<p class="wpmtst-onboarding-doc" ><?php echo sprintf( esc_html__( 'Need help? Check out %s our documentation%s.', 'strong-testimonials' ),  '<a href="https://wpmtst.helpscoutdocs.com/" target="_blank">','</a>' ); ?></p>
+				<p class="wpmtst-onboarding-doc" ><?php echo sprintf( esc_html__( 'Need help? Check out %s our documentation%s.', 'strong-testimonials' ),  '<a href="https://strongtestimonials.com/docs/" target="_blank">','</a>' ); ?></p>
 			</div>
 		</div>
 		<?php
