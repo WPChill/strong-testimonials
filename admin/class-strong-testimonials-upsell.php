e65ac8e44f9b15c66711d06a6c2f61a795b1bdc5
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-08-18 10:10:05 +0300

    [ #407 ] - remove if business

--- admin/class-strong-testimonials-upsell.php
+++ admin/class-strong-testimonials-upsell.php
@@ -880,7 +880,7 @@ class Strong_Testimonials_Upsell {
 			return $pages;
 		}
 		// Add the lite vs pro page only if the user has not purchased the agency package.
-		if ( false === strpos( $packages['current_package']['slug'], 'agency' ) ) {
+		if ( false === strpos( $packages['current_package']['slug'], 'business' ) ) {
 			$pages[92] = $this->get_submenu();
 		}
 
